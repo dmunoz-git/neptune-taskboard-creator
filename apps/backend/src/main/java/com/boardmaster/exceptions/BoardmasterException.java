f2bdd2a8982f328b134af6e0255cf00a988510b8
dmunoz-git <dmunozGit@gmail.com>
2024-06-13 00:07:36 +0200

    feat(exceptions): create basic exception handler

---
+++ apps/backend/src/main/java/com/boardmaster/exceptions/BoardmasterException.java
@@ -0,0 +1,16 @@
+package com.boardmaster.exceptions;
+
+import java.io.Serial;
+
+public class BoardmasterException extends Exception{
+    @Serial
+    private static final long serialVersionUID=1L;
+
+    public BoardmasterException(String message) {
+        super(message);
+    }
+
+    public BoardmasterException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
