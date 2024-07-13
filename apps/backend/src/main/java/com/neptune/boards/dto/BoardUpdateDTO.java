2dada15bbac7fdf2b25b5e628694e770a4aaec9a
dmunoz-git <dmunozGit@gmail.com>
2024-07-13 15:10:48 +0200

    refactor(com.neptune.boards.dto.BoardUpdateDTO, com.neptune.boards.tests.unit.service.BoardServiceTest): update DTO and board service tests

--- apps/backend/src/main/java/com/neptune/boards/dto/BoardUpdateDTO.java
+++ apps/backend/src/main/java/com/neptune/boards/dto/BoardUpdateDTO.java
@@ -1,10 +1,14 @@
 package com.neptune.boards.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@Builder
 public class BoardUpdateDTO {
     private String name;
     private String description;
--- apps/backend/src/test/java/com/neptune/boards/tests/unit/service/BoardServiceTest.java
+++ apps/backend/src/test/java/com/neptune/boards/tests/unit/service/BoardServiceTest.java
@@ -1,5 +1,6 @@
 package com.neptune.boards.tests.unit.service;
 
+import com.neptune.boards.dto.BoardUpdateDTO;
 import com.neptune.boards.entity.Board;
 import com.neptune.boards.exception.NeptuneBoardsException;
 import com.neptune.boards.repository.BoardRepository;
@@ -9,7 +10,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
 
 import java.time.LocalDate;
 import java.util.Arrays;
@@ -35,8 +38,8 @@ public class BoardServiceTest {
     }
 
     @Test
-    @DisplayName("Create Dashboard: should create and return a new dashboard")
-    void createDashboardTest() {
+    @DisplayName("Create Dashboard: should create and return a new board")
+    void createBoardTest() {
         Board board = Board.builder()
                 .id(1L)
                 .UUID(UUID.randomUUID())
@@ -72,8 +75,8 @@ public class BoardServiceTest {
     }
 
     @Test
-    @DisplayName("Get Dashboard by UUID: should return dashboard if found")
-    void getDashboardByIdTest() throws NeptuneBoardsException {
+    @DisplayName("Get Dashboard by UUID: should return board")
+    void getBoardByUUIDTest() throws NeptuneBoardsException {
         Board board = Board.builder().UUID(UUID.randomUUID()).name("Test Dashboard").build();
         when(repository.findByUUID(board.getUUID())).thenReturn(Optional.of(board));
 
@@ -84,25 +87,51 @@ public class BoardServiceTest {
     }
 
     @Test
-    @DisplayName("Exception Board: get board not found")
-    void testNotFoundBoard() {
+    @DisplayName("Exception Board: should return 404 not found board")
+    void getNotFoundBoardTest() {
         NeptuneBoardsException exception = assertThrows(NeptuneBoardsException.class, () -> {
             service.getBoard(UUID.randomUUID());
         });
         assertEquals("Board not found", exception.getMessage());
+        assertEquals(HttpStatus.NOT_FOUND, exception.getStatus());
     }
 
     @Test
     @DisplayName("Get All Dashboards: should return list of dashboards")
-    void getAllDashboardsTest() {
-        Board dashboard1 = Board.builder().name("Dashboard 1").build();
-        Board dashboard2 = Board.builder().name("Dashboard 2").build();
-        when(repository.findAll()).thenReturn(Arrays.asList(dashboard1, dashboard2));
+    void getAllBoardsTest() {
+        // Create boards
+        Board board1 = Board.builder().name("Dashboard 1").build();
+        Board board2 = Board.builder().name("Dashboard 2").build();
 
-        List<Board> dashboards = service.getAllBoards();
+        // Mock repo
+        when(repository.findAll()).thenReturn(Arrays.asList(board1, board2));
 
-        assertNotNull(dashboards);
-        assertEquals(2, dashboards.size());
+        // Get boards
+        List<Board> boards = service.getAllBoards();
+        assertNotNull(boards);
+        assertEquals(2, boards.size());
         verify(repository, times(1)).findAll();
+    }
+
+    @Test
+    @DisplayName("Update Board: should update a board if some of the updatable fields are null")
+    void updateBoardTest() throws NeptuneBoardsException {
+        // Create the updated board data and the actual board
+        BoardUpdateDTO boardRequest = BoardUpdateDTO.builder().name("Updated name").build();
+        UUID boardUUID = UUID.randomUUID();
+        Board board = Board.builder().UUID(boardUUID).name("Old name").description("This is a description").build();
+
+        // Mock repository methods
+        Mockito.when(repository.findByUUID(boardUUID)).thenReturn(Optional.of(board));
+        Mockito.when(repository.save(any(Board.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        // Call the service method
+        Board updatedBoard = service.updateBoard(boardUUID, boardRequest);
+
+        // Verify the results
+        assertEquals(boardRequest.getName(), updatedBoard.getName());
+        assertNotNull(updatedBoard.getDescription());
+        assertEquals(board.getDescription(), updatedBoard.getDescription());
+        assertNotEquals(updatedBoard.getUpdatedAt(), updatedBoard.getCreatedAt());
     }
 }
